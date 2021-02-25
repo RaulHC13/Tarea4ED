13abfe2e4b12e186cbf472751bbcb708d4dd8075
Raúl Herrerías Corral <raulhc1313@gmail.com>
2021-02-25 13:13:47 +0100

    Utiliza las funciones de refactorización para sustituir el código que se encarga de validar una fecha por un método que llamarás "validar", que será público, de forma que se incluyan los comentarios para la documentación. Muestra cómo se hace en los dos entornos. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- Tarea4ED/src/fecha/Fecha.java
+++ Tarea4ED/src/fecha/Fecha.java
@@ -4,60 +4,70 @@
 package fecha;
 
 /**
-* @author Raul
-*
-*/
+ * @author Raul
+ *
+ */
 public class Fecha {
 
-public int dia;
-public int mes;
-public int año;
+	public int dia;
+	public int mes;
+	public int año;
 
-public Fecha(int dia, int mes, int año) {
-this.dia = dia;
-this.mes = mes;
-this.año = año;
-}
+	public Fecha(int dia, int mes, int año) {
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
+	}
 
-public boolean valida() {
+	public boolean valida() {
 
-if (dia < 1 || dia > 31)
-return false;
-if (mes < 1 || mes > 12)
-return false;
+		if (dia < 1 || dia > 31)
+			return false;
+		if (mes < 1 || mes > 12)
+			return false;
 
 // Determinamos la cantidad de días del mes:
-int diasMes = 0;
-switch (mes) {
-case 1:
-case 3:
-case 5:
-case 7:
-case 8:
-case 10:
-case 12:
-diasMes = 31;
-break;
-case 4:
-case 6:
-case 9:
-case 11:
-diasMes = 30;
-break;
-case 2: // Verificamos si el año es bisiesto
-if ((año % 400 == 0) || ((año % 4 == 0) && (año % 100 != 0)))
-diasMes = 29;
-else
-diasMes = 28;
-break;
+		int diasMes = 0;
+		switch (mes) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			diasMes = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			diasMes = 30;
+			break;
+		case 2: // Verificamos si el año es bisiesto
+			if ((año % 400 == 0) || ((año % 4 == 0) && (año % 100 != 0)))
+				diasMes = 29;
+			else
+				diasMes = 28;
+			break;
 
-}
+		}
 
-if (dia > diasMes)
-return false;
-else
-return true;
+		return validar(diasMes);
+		/*
+		 * Comprueba que el número de dia es inferior al número maximo de dias de un mes
+		 */
+	}
 
-}
+	/**
+	 * @param diasMes es el número máximo de dias en un mes
+	 * @return La fecha será valida o no válida
+	 */
+	public boolean validar(int diasMes) {
+		if (dia > diasMes)
+			return false;
+		else
+			return true;
+	}
 
 }
